2938b2b50f03d36ff5947a9cbc78da919cc697ab
Pradhum80 <kumarroney154@gmail.com>
2026-02-20 12:09:45 +0530

    Intro : Js

---
+++ intro1.js
@@ -0,0 +1,25 @@
+//  Java Script is Programming language we use it to give instruction to the computer.
+
+console.log("Apna College");
+
+console.log("Pradhum Kumar");
+
+// Variable.js :- 
+// variables are container for data.
+// Rules:
+//  Variables names are case Sensitive ; "a", & "A" is different
+// Only letters,digit,undersocre(_)and $ is allowed. not even space
+// reserved word cannot be variable names.
+
+name = "Pradhum Kumar";
+console.log(name);
+age = 26
+console.log(age);
+price = 99.99;
+console.log(price);
+
+
+x = null;
+console.log(null);
+y = undefined;
+console.log(y);
