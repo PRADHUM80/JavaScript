ec3e0d1dc2ed5420ab37646a04748df4c8ccc99e
Pradhum80 <kumarroney154@gmail.com>
2026-02-20 16:12:25 +0530

    Operators : Conditional

---
+++ Opertors4.js
@@ -0,0 +1,51 @@
+//  Operators... Used to perform some operation on data.
+
+// 1. Arithmetic Operators:-
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b = ",  a + b);
+// console.log("a - b = ",  a - b);
+// console.log("a * b = ",  a * b);
+// console.log("a / b = ",  a / b);
+// console.log("a % b = ",  a % b);
+// console.log("a ** b = ",  a ** b);
+
+
+// 2. Assignment Operators:-
+// let a = 5;
+// let b = 2;
+
+// a += 5
+// console.log(a);
+
+// b -= 2
+// console.log(b);
+
+
+// 3. Comparision Operators:-
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5 == 2", a == b);
+// console.log("5 != 2", a != b);
+// console.log("5 === 2", a === b); // strict version.. datatype chck. number and string comparisions...
+
+
+// 4. Logical Operators..
+
+let a  = 6;
+let b = 5;
+
+// let cond1 = a > b; //true
+// let cond2 = a === 6; // true
+// console.log("cond1 && cond2 = ", cond1 && cond2);
+
+// or..
+
+console.log("cond1 || cond2 = ", a < b || a === 6); // true
+
+
+
+
---
+++ conditional5.js
@@ -0,0 +1,61 @@
+// Conditional Statements..
+// 1. if
+
+// let age = 25;
+// if(age >= 18) {
+//     console.log("you can cote");
+// }
+
+
+// Q.
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark") {
+//     color = "black";
+// }
+// if(mode === "light") {
+//     color = "white";
+// }
+
+// console.log(color);
+
+
+// 2. if-else Statement..
+// let age1 = 25;
+
+// if(age1 >= 25) {
+//     console.log("vote");
+// } else {
+//     console.log("not vote");
+// }
+
+
+//  Q..
+// let num = 10;
+
+// if(num % 2 === 0) {
+//     console.log(num, "is even");
+// } else {
+//     console.log(num, "is odd");
+// }
+
+
+
+// Ternary Operators..
+
+
+
+// Prompt.....
+// let name = prompt("hello");
+// console.log(name);
+
+let num = prompt("Enter a number");
+if(num % 5 === 0) {
+    console.log(num, "is a Multiple of 5");
+} else {
+    console.log(num, "is not multipele of 5");
+}
+
+
+
