440f761a3e370ea266ca8e4e62edc953bd3c0781
Pradhum80 <kumarroney154@gmail.com>
2026-02-20 21:38:50 +0530

    String : Js

---
+++ String7.js
@@ -0,0 +1,27 @@
+// String :- String is a sequence of characters used to represent text
+// create.
+// let str = "Apna College";
+// console.log(str[0]);
+
+
+// Templates Literals in js..
+// A way to have embedded expressions in strings.
+// `this is a template literal`
+// `${expression}`
+
+// let obj = {
+//     item : "pen",
+//     price : 10,
+// };
+// console.log("the cost of", obj.item, "is", obj.price, "rupees");  // combine of single type so use templete....
+// let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+// let specialString = `This is a template Literal`;
+// console.log(specialString);
+
+
+// Ques..
+let name = prompt("enter your full name");
+console.log(name);
+
+let username = "@" + name + name.length;
+console.log(username);
