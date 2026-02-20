2ef1996204e23d50bfac1f269762a8bee6b69788
Pradhum80 <kumarroney154@gmail.com>
2026-02-20 22:24:15 +0530

    Function : Js

---
+++ Function9.js
@@ -0,0 +1,30 @@
+// Function :- block of code that perform specific task can be invoked whenever needed.
+// create..
+// function myfunction(){
+//     console.log("Welcome to Apna College");
+//     console.log("We are learning JS");
+// }
+// // call functionName..
+// myfunction();
+
+
+// function myFunction(msg) { // parameter msg
+//     console.log(msg);
+// }
+// myFunction("i Love JS"); // arguments
+
+
+// sum 2 num.
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+// sum(1, 2);
+// ORRRR
+function sum(x, y){
+    s = x + y;
+    return s;
+}
+let val = sum(3, 4);
+console.log(val);
+
