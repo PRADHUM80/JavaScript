b5fb48225379eb02021bf48a4845b54ec46356c5
Pradhum80 <kumarroney154@gmail.com>
2026-02-20 22:01:06 +0530

    Arrays : Methods

---
+++ Array8.js
@@ -0,0 +1,25 @@
+// Array:- Collections of items - linear
+
+let marks = [97,95, 96, 92];
+// console.log(marks);
+// console.log(marks.length);
+
+// print all marks...
+// for(let idx=0; idx<marks.length; idx++) {
+//     console.log(marks[idx]);
+// }
+
+
+// for of .
+
+// for(let i of marks) {
+//     console.log(i);
+// }
+
+// push()
+
+let veg = ["potato", "apple", "litchi", "tomato"];
+
+// pop()
+
+// toString()
